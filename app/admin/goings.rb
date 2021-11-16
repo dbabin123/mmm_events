59d209a4c7280090b4329cae0d31dba93040d33f
anonymous anonymous <david.babin@kellogg.northwestern.edu>
2021-11-16 21:24:56 +0000

    Whitelist columns in Going for ActiveAdmin

--- app/admin/goings.rb
+++ app/admin/goings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Going do
 
+ permit_params :profile_id, :event_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
