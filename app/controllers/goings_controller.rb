ceeab954cd6e1241eb61b193d04187fb98b41b51
anonymous anonymous <david.babin@kellogg.northwestern.edu>
2021-11-16 21:25:05 +0000

    Display associated collections on show pages

--- app/controllers/goings_controller.rb
+++ app/controllers/goings_controller.rb
@@ -42,8 +42,14 @@ class GoingsController < ApplicationController
   # DELETE /goings/1
   def destroy
     @going.destroy
-    redirect_to goings_url, notice: 'Going was successfully destroyed.'
+    message = "Going was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to goings_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
